bf3f8a2980060cb229df6cda3ab52cba7347324c
Kisa <117823800+kisatt@users.noreply.github.com>
2024-06-22 22:02:34 +0700

    Update TaiTro.java

--- src/MODEL/TaiTro.java
+++ src/MODEL/TaiTro.java
@@ -1,4 +1,4 @@
-import java.io.BufferedReader;
+  import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.*;
@@ -7,86 +7,110 @@ import java.util.Iterator;
 
 import javax.swing.JOptionPane;
 
-public class DoiTac {
-    private static String name;
-    private static String nameHost;
-    private static String phone;
-    private static String email;
+public class TaiTro{
 
-    
-    public static String getName() {
+    private int id = 0;
+    private String name;
+    private String nameHost;
+    private String phone;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
         return name;
     }
 
-    public static void setName(String name) {
-        DoiTac.name = name;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public static String getPhone() {
-        return phone;
-    }
-
-    public static void setPhone(String phone) {
-        DoiTac.phone = phone;
-    }
-
-    public static String getEmail() {
-        return email;
-    }
-
-    public static void setEmail(String email) {
-        DoiTac.email = email;
-    }
-
-    public static String getNameHost() {
+    public String getNameHost() {
         return nameHost;
     }
 
-    public static void setNameHost(String nameHost) {
-        DoiTac.nameHost = nameHost;
+    public void setNameHost(String nameHost) {
+        this.nameHost = nameHost;
     }
 
+    public String getPhone() {
+        return phone;
+    }
 
-    public DoiTac(String name, String nameHost, String phone, String email){
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public TaiTro(){}
+
+    public TaiTro(String name, String nameHost, String phone, String email){
+        setId(id);
         setName(name);
         setNameHost(nameHost);
         setPhone(phone);
         setEmail(email);
     }
 
-    public static DoiTac find(ArrayList<DoiTac> list, String name){
-        Iterator<DoiTac> it = list.iterator();
+}
+
+class ListTaiTro extends TaiTro{
+
+    ArrayList<TaiTro> list;
+
+    public ListTaiTro(){
+        super();
+        list = new ArrayList<>();
+    }
+
+    public TaiTro find(String name){
+        Iterator<TaiTro> it = list.iterator();
         while(it.hasNext()){
-            DoiTac temp = it.next();
+            TaiTro temp = it.next();
             if(temp.getName().contains(name))
                 return temp;
         }
         return null;
     }
 
-    public static void importFile(BufferedReader file, ArrayList<DoiTac> list) throws IOException{
+    public void importFile(BufferedReader file) throws IOException{
         String line;
         while ((line = file.readLine()) != null) {
             String[] value = line.split("\t");
-            list.add(new DoiTac(value[0], value[1], value[2], value[3]));
+            list.add(new TaiTro(value[0], value[1], value[2], value[3]));
         }
     }
 
-    public static void  exportFile(PrintWriter file, ArrayList<DoiTac> list){
-        for (DoiTac doiTac : list) {
-            file.write(doiTac.getName() + "\t" + doiTac.getNameHost() + "\t" + doiTac.getPhone() + "\t" + doiTac.getEmail());
+    public void  exportFile(PrintWriter file){
+        for (TaiTro taiTro : list) {
+            file.write(taiTro.getName() + "\t" + taiTro.getNameHost() + "\t" + taiTro.getPhone() + "\t" + taiTro.getEmail());
         }
     }
     
-    public static void addList(ArrayList<DoiTac> list, DoiTac doiTac){
-        if (!doiTac.getName().isEmpty() && !doiTac.getNameHost().isEmpty() && !doiTac.getEmail().isEmpty()){
+    public void addList(TaiTro taiTro){
+        if (!taiTro.getName().isEmpty() && !taiTro.getNameHost().isEmpty() && !taiTro.getEmail().isEmpty()){
             if (Check.isPhoneNumber(getPhone())){
-                list.add(doiTac);
+                list.add(taiTro);
             }else JOptionPane.showMessageDialog(null,"Check Phone Number", "Try again",JOptionPane.ERROR_MESSAGE);
         } else JOptionPane.showMessageDialog(null,"Information is not valid\nPlease enter all fields", "Try again",JOptionPane.ERROR_MESSAGE);
     }
 
-    
+    public ArrayList outList(){
+        return list;
+    }
+
 }
-
-
+ 
