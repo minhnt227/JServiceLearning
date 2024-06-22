76426a4c4e48a5ca4260310e66d7ed521dd7f00b
Kisa <117823800+kisatt@users.noreply.github.com>
2024-06-22 16:45:06 +0700

    Update TaiTro.java

--- src/MODEL/TaiTro.java
+++ src/MODEL/TaiTro.java
@@ -77,11 +77,11 @@ public class TaiTro{
         return false;
     }
 
-    public static TaiTro find(ArrayList<TaiTro> list, int id){
+    public static TaiTro find(ArrayList<TaiTro> list, String name){
         Iterator<TaiTro> it = list.iterator();
         while(it.hasNext()){
             TaiTro temp = it.next();
-            if(temp.getId() == id )
+            if(temp.getName().contains(name))
                 return temp;
         }
         return null;
@@ -97,13 +97,13 @@ public class TaiTro{
 
     public static void  exportFile(PrintWriter file, ArrayList<TaiTro> list){
         for (TaiTro taiTro : list) {
-            file.write(taiTro.getName() + "\t" + taiTro.getNameHost() + "\t" + taiTro.getPhone() + "\t" + taiTro.getEmail() + "\t" + taiTro.getId());
+            file.write(taiTro.getName() + "\t" + taiTro.getNameHost() + "\t" + taiTro.getPhone() + "\t" + taiTro.getEmail());
         }
     }
     
     public static void addList(ArrayList<TaiTro> list, TaiTro taiTro){
         if (!taiTro.getName().isEmpty() && !taiTro.getNameHost().isEmpty() && !taiTro.getEmail().isEmpty()){
-            if (Check.isNumber(getPhone()){
+            if (Check.isPhoneNumber(getPhone())){
                 list.add(taiTro);
             }else JOptionPane.showMessageDialog(null,"Check Phone Number", "Try again",JOptionPane.ERROR_MESSAGE);
         } else JOptionPane.showMessageDialog(null,"Information is not valid\nPlease enter all fields", "Try again",JOptionPane.ERROR_MESSAGE);
@@ -112,3 +112,4 @@ public class TaiTro{
     
 
 }
+ 
