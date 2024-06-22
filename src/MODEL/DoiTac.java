dee50e5e43bc34590dac0d9d773000d3917cbb06
Kisa <117823800+kisatt@users.noreply.github.com>
2024-06-22 20:51:46 +0700

    Update DoiTac.java

--- src/MODEL/DoiTac.java
+++ src/MODEL/DoiTac.java
@@ -8,43 +8,45 @@ import java.util.Iterator;
 import javax.swing.JOptionPane;
 
 public class DoiTac {
-    private static String name;
-    private static String nameHost;
-    private static String phone;
-    private static String email;
+    private String name;
+    private String nameHost;
+    private String phone;
+    private String email;
 
     
-    public static String getName() {
+    public String getName() {
         return name;
     }
 
-    public static void setName(String name) {
-        DoiTac.name = name;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public static String getPhone() {
-        return phone;
-    }
-
-    public static void setPhone(String phone) {
-        DoiTac.phone = phone;
-    }
-
-    public static String getEmail() {
-        return email;
-    }
-
-    public static void setEmail(String email) {
-        DoiTac.email = email;
-    }
-
-    public static String getNameHost() {
+    public String getNameHost() {
         return nameHost;
     }
 
-    public static void setNameHost(String nameHost) {
-        DoiTac.nameHost = nameHost;
+    public void setNameHost(String nameHost) {
+        this.nameHost = nameHost;
     }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public DoiTac(){}
 
 
     public DoiTac(String name, String nameHost, String phone, String email){
@@ -54,7 +56,17 @@ public class DoiTac {
         setEmail(email);
     }
 
-    public static DoiTac find(ArrayList<DoiTac> list, String name){
+    
+}
+
+class ListDoiTac extends DoiTac{
+    ArrayList<DoiTac> list;
+
+    public ListDoiTac(){
+        list = new ArrayList<>();
+    }
+
+    public DoiTac find(String name){
         Iterator<DoiTac> it = list.iterator();
         while(it.hasNext()){
             DoiTac temp = it.next();
@@ -64,21 +76,7 @@ public class DoiTac {
         return null;
     }
 
-    public static void importFile(BufferedReader file, ArrayList<DoiTac> list) throws IOException{
-        String line;
-        while ((line = file.readLine()) != null) {
-            String[] value = line.split("\t");
-            list.add(new DoiTac(value[0], value[1], value[2], value[3]));
-        }
-    }
-
-    public static void  exportFile(PrintWriter file, ArrayList<DoiTac> list){
-        for (DoiTac doiTac : list) {
-            file.write(doiTac.getName() + "\t" + doiTac.getNameHost() + "\t" + doiTac.getPhone() + "\t" + doiTac.getEmail());
-        }
-    }
-    
-    public static void addList(ArrayList<DoiTac> list, DoiTac doiTac){
+    public void addList(DoiTac doiTac){
         if (!doiTac.getName().isEmpty() && !doiTac.getNameHost().isEmpty() && !doiTac.getEmail().isEmpty()){
             if (Check.isPhoneNumber(getPhone())){
                 list.add(doiTac);
@@ -86,7 +84,18 @@ public class DoiTac {
         } else JOptionPane.showMessageDialog(null,"Information is not valid\nPlease enter all fields", "Try again",JOptionPane.ERROR_MESSAGE);
     }
 
-    
+    public void importFile(BufferedReader file) throws IOException{
+        String line;
+        while ((line = file.readLine()) != null) {
+            String[] value = line.split("\t");
+            list.add(new DoiTac(value[0], value[1], value[2], value[3]));
+        }
+    }
+
+    public void  exportFile(PrintWriter file){
+        for (DoiTac doiTac : list) {
+            file.write(doiTac.getName() + "\t" + doiTac.getNameHost() + "\t" + doiTac.getPhone() + "\t" + doiTac.getEmail());
+        }
+    }
+
 }
-
-
