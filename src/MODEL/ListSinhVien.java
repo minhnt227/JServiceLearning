74e67eca0caf1cf1c2f3ff09e4ba7bc6565ed132
Kisa <117823800+kisatt@users.noreply.github.com>
2024-06-24 21:52:41 +0700

    Update ListSinhVien.java

--- src/MODEL/ListSinhVien.java
+++ src/MODEL/ListSinhVien.java
@@ -22,7 +22,7 @@ public class ListSinhVien extends SinhVien{
         list = new ArrayList<>();
     }
 
-    public SinhVien find(String nameKhoa){
+    public static SinhVien find(String nameKhoa){
         Iterator<SinhVien> it = list.iterator();
         while(it.hasNext()){
             SinhVien temp = it.next();
@@ -32,7 +32,7 @@ public class ListSinhVien extends SinhVien{
         return null;
     }
 
-    public int countKhoa(String nameKhoa){
+    public static int countKhoa(String nameKhoa){
         int cnt = 0;
         for (SinhVien sinhVien : list) {
             if (find(nameKhoa) != null) cnt++;
@@ -40,11 +40,11 @@ public class ListSinhVien extends SinhVien{
         return cnt;
     }
 
-    public int soLuong(){
+    public static int soLuong(){
         return list.size();
     }
 
-    public void importFile(BufferedReader file) throws IOException{
+    public static void importFile(BufferedReader file) throws IOException{
         String line;
         while ((line = file.readLine()) != null) {
             String[] value = line.split("\t");
@@ -52,19 +52,19 @@ public class ListSinhVien extends SinhVien{
         }
     }
 
-    public void  exportFile(PrintWriter file){
+    public static void  exportFile(PrintWriter file){
         for (SinhVien sv : list) {
             file.write(sv.getId() + "\t" + sv.getName() + "\t" + sv.getNameKhoa() + "\t" + sv.getIdKhoa());
         }
     }
     
-    public void addList(SinhVien sv){
+    public static void addList(SinhVien sv){
         if (!sv.getName().isEmpty() && !sv.getId().isEmpty() && !sv.getNameKhoa().isEmpty()){
             list.add(sv);
         } else JOptionPane.showMessageDialog(null,"Information is not valid\nPlease enter all fields", "Try again",JOptionPane.ERROR_MESSAGE);
     }
 
-    public ArrayList outList(){
+    public static ArrayList outList(){
         return list;
     }
 
