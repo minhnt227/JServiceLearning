13a54ca4e049c834675b8cd5816eb70f4dd08720
Kisa <117823800+kisatt@users.noreply.github.com>
2024-06-25 16:05:21 +0700

    Update ListDoiTac.java

--- src/MODEL/ListDoiTac.java
+++ src/MODEL/ListDoiTac.java
@@ -6,6 +6,7 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Iterator;
 import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -13,11 +14,41 @@ import javax.swing.JOptionPane;
  */
 public class ListDoiTac extends DoiTac{
     public static ArrayList<DoiTac> list;
+    public Object[] colHeader;
+    public DefaultTableModel model;
 
     public ListDoiTac(){
         list = new ArrayList<>();
+        model = new DefaultTableModel(colHeader,0);
     }
 
+    public DefaultTableModel getTableModel(){
+        Iterator<DoiTac> it = list.iterator();
+        while(it.hasNext()){
+            DoiTac temp = it.next();
+            model.addRow(new Object[]{temp.getName(), temp.getNameHost(), temp.getPhone(), temp.getEmail()});
+        }
+        return model;
+    }
+    
+    public static ArrayList exportList(DefaultTableModel Model){
+        ArrayList<DoiTac> listDT = new ArrayList<>();
+        
+        int rowCount = Model.getRowCount();
+
+        for (int row = 0; row < rowCount; row++) {
+            DoiTac dt = new DoiTac();
+            dt.setName((String) Model.getValueAt(row, 0));
+            dt.setNameHost((String) Model.getValueAt(row, 1));
+            dt.setEmail((String) Model.getValueAt(row, 2));
+            dt.setPhone((String) Model.getValueAt(row, 3));
+            listDT.add(dt);
+        }
+        
+        return listDT;
+    }
+    
+    
     public static DoiTac find(String name){
         Iterator<DoiTac> it = list.iterator();
         while(it.hasNext()){
