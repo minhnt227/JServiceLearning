16f12932137205ad74eb418214a5fe1962a75d82
Kisa <117823800+kisatt@users.noreply.github.com>
2024-06-25 16:07:22 +0700

    Update ListSinhVien.java

--- src/MODEL/ListSinhVien.java
+++ src/MODEL/ListSinhVien.java
@@ -11,51 +11,79 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
  * @author Kisa
  */
-public class ListSinhVien extends SinhVien {
-
+public class ListSinhVien extends SinhVien{
     public static ArrayList<SinhVien> list;
+    public Object[] colHeader;
+    public DefaultTableModel model;
 
-    public ListSinhVien() {
+    public  ListSinhVien(){
         list = new ArrayList<>();
+        model = new DefaultTableModel(colHeader,0);
     }
-
-    public ListSinhVien(int lim, String ID, String FullName, String FalcutyID, boolean Hide) throws SQLException {
-        ListSinhVien temp = ( new DAO.DAO_SinhVien() ).getListFromDB(lim, ID, FullName, FalcutyID, true);
+    
+    public ListSinhVien(int lim, String ID, String FullName, String FalcutyID, boolean Hide) throws SQLException{
+        ListSinhVien temp = (new DAO.DAO_SinhVien()).getListFromDB(lim, ID, FullName, FalcutyID, true);
         list = temp.list;
         colHeader = temp.getColHeader();
     }
-
-    public static SinhVien find(String nameKhoa) {
+    
+    public DefaultTableModel getTableModel(){
         Iterator<SinhVien> it = list.iterator();
-        while (it.hasNext()) {
+        while(it.hasNext()){
             SinhVien temp = it.next();
-            if (temp.getNameKhoa().contains(nameKhoa)) {
+            model.addRow(new Object[]{temp.getId(), temp.getName(), temp.getNameKhoa(), temp.getIdKhoa()});
+        }
+        return model;
+    }
+    
+    
+    public static ArrayList exportList(DefaultTableModel Model){
+        ArrayList<SinhVien> listSV = new ArrayList<>();
+        
+        int rowCount = Model.getRowCount();
+
+        for (int row = 0; row < rowCount; row++) {
+            SinhVien sv = new SinhVien();
+            sv.setId((String) Model.getValueAt(row, 0));
+            sv.setName((String) Model.getValueAt(row, 1));
+            sv.setIdKhoa((String) Model.getValueAt(row, 2));
+            sv.setNameKhoa((String) Model.getValueAt(row, 3));
+            listSV.add(sv);
+        }
+        
+        return listSV;
+    }
+    
+
+    public static SinhVien find(String nameKhoa){
+        Iterator<SinhVien> it = list.iterator();
+        while(it.hasNext()){
+            SinhVien temp = it.next();
+            if(temp.getNameKhoa().contains(nameKhoa))
                 return temp;
-            }
         }
         return null;
     }
 
-    public static int countKhoa(String nameKhoa) {
+    public static int countKhoa(String nameKhoa){
         int cnt = 0;
         for (SinhVien sinhVien : list) {
-            if (find(nameKhoa) != null) {
-                cnt++;
-            }
+            if (find(nameKhoa) != null) cnt++;
         }
         return cnt;
     }
-    
-    public static int soLuong() {
+
+    public static int soLuong(){
         return list.size();
     }
 
-    public static void importFile(BufferedReader file) throws IOException {
+    public static void importFile(BufferedReader file) throws IOException{
         String line;
         while ((line = file.readLine()) != null) {
             String[] value = line.split("\t");
@@ -63,21 +91,19 @@ public class ListSinhVien extends SinhVien {
         }
     }
 
-    public static void exportFile(PrintWriter file) {
+    public static void  exportFile(PrintWriter file){
         for (SinhVien sv : list) {
             file.write(sv.getId() + "\t" + sv.getName() + "\t" + sv.getNameKhoa() + "\t" + sv.getIdKhoa());
         }
     }
-
-    public static void addList(SinhVien sv) {
-        if (!sv.getName().isEmpty() && !sv.getId().isEmpty() && !sv.getNameKhoa().isEmpty()) {
+    
+    public static void addList(SinhVien sv){
+        if (!sv.getName().isEmpty() && !sv.getId().isEmpty() && !sv.getNameKhoa().isEmpty()){
             list.add(sv);
-        } else {
-            JOptionPane.showMessageDialog(null, "Information is not valid\nPlease enter all fields", "Try again", JOptionPane.ERROR_MESSAGE);
-        }
+        } else JOptionPane.showMessageDialog(null,"Information is not valid\nPlease enter all fields", "Try again",JOptionPane.ERROR_MESSAGE);
     }
 
-    public static ArrayList outList() {
+    public static ArrayList outList(){
         return list;
     }
 
